5da935de791487b9d9c50c43e69c24725612fb7d
Alka Rani <70084670+Alka-2022@users.noreply.github.com>
2021-05-01 17:19:02 +0530

    Delete Richest Customer Wealth.py

--- DAY-6/Richest Customer Wealth.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        comp_total = []
-        for i in accounts:
-            total = sum(i)
-            comp_total.append(total)
-        return max(comp_total)
